e52ac55bc52afe0afb56303e802a740dd923552c
kkkofi <kofiababio07@gmail.com>
2024-07-03 23:24:04 +0000

    added some to OurStory.js

--- fashion-app/components/OurStory.js
+++ fashion-app/components/OurStory.js
@@ -26,56 +26,56 @@ const OurStory = ({navigation}) => {
           image: require('./assets/dress1.png'),
           title: 'Office Wear',
           description: 'reversible angora cardigan',
-          price: '$120',
+          amount: '$120',
         },
         {
           id: '2',
           image: require('./assets/dress2.png'),
           title: 'Black',
           description: 'reversible angora cardigan',
-          price: '$120',
+          amount: '$120',
         },
         {
           id: '3',
           image: require('./assets/dress3.png'),
           title: 'Church Wear',
           description: 'reversible angora cardigan',
-          price: '$120',
+          amount: '$120',
         },
         {
           id: '4',
           image: require('./assets/dress4.png'),
           title: 'Lamerei',
           description: 'reversible angora cardigan',
-          price: '$120',
+          amount: '$120',
         },
         {
           id: '5',
           image: require('./assets/dress5.png'),
           title: '21WN',
           description: 'reversible angora cardigan',
-          price: '$120',
+          amount: '$120',
         },
         {
           id: '6',
           image: require('./assets/dress6.png'),
           title: 'Lopo',
           description: 'reversible angora cardigan',
-          price: '$120',
+          amount: '$120',
         },
         {
             id: '7',
             image: require('./assets/dress7.png'),
             title: '21WN',
             description: 'reversible angora cardigan',
-            price: '$120',
+            amount: '$120',
           },
           {
             id: '8',
             image: require('./assets/dress3.png'),
             title: 'lame',
             description: 'reversible angora cardigan',
-            price: '$120',
+            amount: '$120',
           },
       ];
 
@@ -97,7 +97,7 @@ const OurStory = ({navigation}) => {
                     <View>
                         <Text style={{fontWeight:'bold',fontSize:20,}}>{item.title}</Text>
                         <Text style={{marginTop:3}}>{item.description}</Text>
-                        <Text style={{color:'#dd8560', fontSize:20,marginTop:2}}>{item.price}</Text>
+                        <Text style={{color:'#dd8560', fontSize:20,marginTop:2}}>{item.amount}</Text>
                     </View>
                     
 
